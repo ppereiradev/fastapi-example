988c71ae1571aff86836faedc40503d74cb7eee1
Paulo Pereira <ppereira.dev@gmail.com>
2025-03-11 14:37:32 -0300

    chore: add /api/v1 in all endpoints

--- projeto/app/main.py
+++ projeto/app/main.py
@@ -1,20 +1,22 @@
 import uuid as _uuid
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, APIRouter
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.db.init_db import init_db
 from app.api.v1.routers import user, auth
 
 app = FastAPI()
 
-
 @app.on_event("startup")
 async def startup():
     await init_db()  # Criar tabelas no banco ao iniciar o app
 
-app.include_router(user.router, prefix="/users", tags=["users"])
-app.include_router(auth.router, prefix="/auth", tags=["auth"])
 
-@app.get("/")
-async def root():
-    return {"page": "home"}
+# Crie o APIRouter com o prefixo '/api/v1'
+api_v1_router = APIRouter()
 
+# Registre outros routers dentro do router api_v1_router
+api_v1_router.include_router(user.router, prefix="/users", tags=["users"])
+api_v1_router.include_router(auth.router, prefix="/auth", tags=["auth"])
+
+# Inclua o router global na aplicação
+app.include_router(api_v1_router, prefix="/api/v1")
--- projeto/app/services/auth.py
+++ projeto/app/services/auth.py
@@ -1,3 +1,4 @@
+from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.services.user import get_user_by_email_login
 from app.schemas.auth import LoginRequest
@@ -7,7 +8,7 @@ async def get_token(user: LoginRequest, db: AsyncSession) -> str:
     if await authenticate_user(user.email, user.password, db):
         return create_access_token(user.dict())
     else:
-        return None
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Incorrect email or password")
 
 async def authenticate_user(user_email: str, user_password: str, db: AsyncSession) -> bool:
     user_from_db = await get_user_by_email_login(user_email, db)
