bae197eff0ce1f69ec182087d32a8bda72a8112b
Paulo Pereira <ppereira.dev@gmail.com>
2025-03-21 22:24:29 -0300

    refactor: improve the dependecy get_db

--- projeto/app/dependencies/db.py
+++ projeto/app/dependencies/db.py
@@ -1,7 +1,13 @@
 from app.db.session import AsyncSessionLocal
+from sqlalchemy.exc import SQLAlchemyError
 
 
 # Dependência para injetar o DB nas rotas
 async def get_db():
     async with AsyncSessionLocal() as session:
-        yield session
+        try:
+            yield session
+            await session.commit()  # Commit da transação se tudo der certo
+        except SQLAlchemyError as e:
+            await session.rollback()  # Rollback em caso de erro
+            raise
